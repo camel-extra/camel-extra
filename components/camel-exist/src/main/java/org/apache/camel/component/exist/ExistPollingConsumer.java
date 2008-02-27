ed7d9b0da2c72b932c1328f4d23007dfae769595
james.strachan <james.strachan@da7677d9-e527-8880-3e75-8a0c88491609>
2008-02-27 15:03:31 +0000

    tidied up the poms to avoid inheriting from the Camel parent pom (as the new ${pom.version} change breaks things) - and switched the package name of the Esper component
    git-svn-id: http://svn.codespot.com/a/apache-extras.org/camel-extra/trunk@22 da7677d9-e527-8880-3e75-8a0c88491609

---
+++ components/camel-exist/src/main/java/org/apache/camel/component/exist/ExistPollingConsumer.java
@@ -0,0 +1,70 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.exist;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.PollingConsumerSupport;
+import org.xmldb.api.base.Resource;
+import org.xmldb.api.base.ResourceIterator;
+import org.xmldb.api.base.XMLDBException;
+
+/**
+ * @version $Revision: 1.1 $
+ */
+public class ExistPollingConsumer extends PollingConsumerSupport<Exchange> {
+    private ExistEndpoint endpoint;
+    private ResourceIterator iterator;
+
+    public ExistPollingConsumer(ExistEndpoint endpoint) {
+        super(endpoint);
+        this.endpoint = endpoint;
+    }
+
+    public Exchange receive() {
+        try {
+            if (iterator == null) {
+                iterator = endpoint.createResourceIterator();
+            }
+            if (iterator.hasMoreResources()) {
+                Resource resource = iterator.nextResource();
+                return endpoint.createExchange(resource);
+            }
+            else {
+                iterator = null;
+                return null;
+            }
+        }
+        catch (XMLDBException e) {
+            throw new RuntimeExistException(endpoint, e);
+        }
+    }
+
+    public Exchange receiveNoWait() {
+        return receive();
+    }
+
+    public Exchange receive(long l) {
+        return receive();
+    }
+
+    protected void doStart() throws Exception {
+    }
+
+    protected void doStop() throws Exception {
+    }
+}
---
+++ components/camel-exist/src/main/java/org/apache/camel/component/exist/ExistProducer.java
@@ -0,0 +1,49 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.exist;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultProducer;
+import org.w3c.dom.Node;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.modules.XMLResource;
+
+/**
+ * @version $Revision: 1.1 $
+ */
+public class ExistProducer extends DefaultProducer<Exchange> {
+    private ExistEndpoint endpoint;
+
+    public ExistProducer(ExistEndpoint endpoint) {
+        super(endpoint);
+        this.endpoint = endpoint;
+    }
+
+    public void process(Exchange exchange) throws Exception {
+        Collection collection = endpoint.getCollection();
+        XMLResource document = (XMLResource) collection.createResource(null, "XMLResource");
+        Object body = exchange.getIn().getBody();
+        if (body instanceof Node) {
+            document.setContentAsDOM((Node) body);
+        }
+        else {
+            document.setContent(body);
+        }
+        collection.storeResource(document);
+    }
+}
