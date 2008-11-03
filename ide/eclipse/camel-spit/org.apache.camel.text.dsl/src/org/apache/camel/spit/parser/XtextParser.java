f038297c17e2381703f4298ff1a4ab5585525dfc
oisin.hurley <oisin.hurley@da7677d9-e527-8880-3e75-8a0c88491609>
2008-11-03 14:25:11 +0000

    Code drop for the DSL editor. This won't compile in your IDE as-is, it needs generated code to work correctly.
    git-svn-id: http://svn.codespot.com/a/apache-extras.org/camel-extra/trunk@245 da7677d9-e527-8880-3e75-8a0c88491609

---
+++ ide/eclipse/camel-spit/org.apache.camel.text.dsl/src/org/apache/camel/spit/parser/XtextParser.java
@@ -0,0 +1,12 @@
+
+package org.apache.camel.spit.parser;
+
+import java.io.InputStream;
+
+public class XtextParser extends GenParser {
+
+	public XtextParser(InputStream in) {
+		super(in);
+	}
+
+}
