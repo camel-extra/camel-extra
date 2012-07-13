53af9cb080bf891744d9d0658530cb8f02496ef6
hekonsek <hekonsek@da7677d9-e527-8880-3e75-8a0c88491609>
2012-07-13 11:44:11 +0000

    Added even more missing license headers. :)
    git-svn-id: http://svn.codespot.com/a/apache-extras.org/camel-extra/trunk@903 da7677d9-e527-8880-3e75-8a0c88491609

--- ide/eclipse/camel-spit/org.apache.camel.text.dsl/src-gen/org/apache/camel/spit/parser/spitLexer.java
+++ ide/eclipse/camel-spit/org.apache.camel.text.dsl/src-gen/org/apache/camel/spit/parser/spitLexer.java
@@ -1,5 +1,24 @@
-// $ANTLR 3.0 ..//org.apache.camel.text.dsl/src-gen//org/apache/camel/spit/parser/spit.g 2008-11-11 16:35:01
+/**************************************************************************************
+ http://code.google.com/a/apache-extras.org/p/camel-extra
 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ 02110-1301, USA.
+
+ http://www.gnu.org/licenses/gpl-2.0-standalone.html
+ ***************************************************************************************/
 package org.apache.camel.spit.parser;
 
 import org.openarchitectureware.xtext.parser.ErrorMsg;
