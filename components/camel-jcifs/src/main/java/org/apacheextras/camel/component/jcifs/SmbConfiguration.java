ffea184880282da134e073ef57fb631fd1043b77
hekonsek <hekonsek@da7677d9-e527-8880-3e75-8a0c88491609>
2013-09-24 20:49:48 +0000

    Merged from trunk Fixed licenses to match RAT validation rules. [from revision 1082]
    git-svn-id: http://svn.codespot.com/a/apache-extras.org/camel-extra/branches/camel-extra-2.11.x@1083 da7677d9-e527-8880-3e75-8a0c88491609

--- components/camel-jcifs/src/main/java/org/apacheextras/camel/component/jcifs/SmbConfiguration.java
+++ components/camel-jcifs/src/main/java/org/apacheextras/camel/component/jcifs/SmbConfiguration.java
@@ -1,27 +1,31 @@
 /**************************************************************************************
- * jcifs Camel component
- * Copyright (C) 2010 Redpill Linpro AB
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ Copyright (C) 2010 Redpill Linpro AB
+ http://code.google.com/a/apache-extras.org/p/camel-extra
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ 02110-1301, USA.
+
+ http://www.gnu.org/licenses/gpl-2.0-standalone.html
  ***************************************************************************************/
 package org.apacheextras.camel.component.jcifs;
 
-import java.net.URI;
-
 import org.apache.camel.component.file.GenericFileConfiguration;
 import org.apache.camel.util.ObjectHelper;
+
+import java.net.URI;
 
 public class SmbConfiguration extends GenericFileConfiguration {
 
