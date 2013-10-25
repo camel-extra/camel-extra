240877c142d4f11609ce117eb6e51b3eee945024
cemmersb@gmail.com <cemmersb@gmail.com@da7677d9-e527-8880-3e75-8a0c88491609>
2013-10-25 15:06:45 +0000

    + add W3C dom event test
    git-svn-id: http://svn.codespot.com/a/apache-extras.org/camel-extra/trunk@1126 da7677d9-e527-8880-3e75-8a0c88491609

---
+++ components/camel-esper/src/test/java/org/apacheextras/camel/component/esper/EsperDomNodeRouteTest.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.apacheextras.camel.component.esper;
+
+import com.espertech.esper.event.map.MapEventBean;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ *
+ * @author cemmersb
+ */
+public class EsperDomNodeRouteTest extends CamelTestSupport {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(EsperDomNodeRouteTest.class);
+
+  private Document document;
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    document = buildDom();
+  }
+
+  private Document buildDom() {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    Document doc = null;
+    try {
+      DocumentBuilder builder = factory.newDocumentBuilder();
+      doc = builder.newDocument();
+      Element root = doc.createElement("root");
+      doc.appendChild(root);
+      root.appendChild(doc.createTextNode("Hello"));
+      root.appendChild(doc.createTextNode(" "));
+      root.appendChild(doc.createTextNode("World!"));
+    } catch (ParserConfigurationException ex) {
+      LOGGER.error("Could not instantiate document builder: {}", ex.getMessage());
+    }
+    return doc;
+  }
+
+  @Test
+  public void testSendDoDmNodeEvents() throws InterruptedException {
+    MockEndpoint endpoint = getMockEndpoint("mock:results");
+    template.sendBody("direct://start", document);
+    endpoint.expectedMessageCount(1);
+    endpoint.assertIsSatisfied();
+    List<Exchange> exchanges = endpoint.getExchanges();
+    for (Exchange exchange : exchanges) {
+      MapEventBean eventBean = exchange.getIn().getBody(MapEventBean.class);
+      Document doc = (Document) eventBean.get("childNodes");
+      Element element = doc.getDocumentElement();
+      assertTrue(element.toString().contains("root"));
+      assertEquals("root", element.getNodeName());
+      assertEquals("Hello World!", element.getTextContent());
+    }
+  }
+
+  @Override
+  protected RouteBuilder createRouteBuilder() throws Exception {
+    return new RouteBuilder() {
+      @Override
+      public void configure() throws Exception {
+        // Start route
+        from("direct://start")
+                .to("log://esper-dom?level=INFO")
+                .to("esper://esper-dom");
+        // Esper stream route
+        from("esper://esper-dom?eql=insert into DomStream select * from org.w3c.dom.Document")
+                .to("log://esper-dom?level=INFO");
+        // Esper selection route
+        from("esper://esper-dom?eql=select childNodes from DomStream")
+                .to("mock:results");
+      }
+    };
+  }
+}
