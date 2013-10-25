d1122f723d18063e38c9a9005d5a1b5ffdaf6a40
cemmersb@gmail.com <cemmersb@gmail.com@da7677d9-e527-8880-3e75-8a0c88491609>
2013-10-25 15:07:28 +0000

    +/- modify license text
    git-svn-id: http://svn.codespot.com/a/apache-extras.org/camel-extra/trunk@1127 da7677d9-e527-8880-3e75-8a0c88491609

--- components/camel-esper/src/test/java/org/apacheextras/camel/component/esper/EsperDomNodeRouteTest.java
+++ components/camel-esper/src/test/java/org/apacheextras/camel/component/esper/EsperDomNodeRouteTest.java
@@ -1,7 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * ************************************************************************************
+ * http://code.google.com/a/apache-extras.org/p/camel-extra
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * http://www.gnu.org/licenses/gpl-2.0-standalone.html
+ **************************************************************************************
  */
 package org.apacheextras.camel.component.esper;
 
@@ -20,10 +37,6 @@ import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-/**
- *
- * @author cemmersb
- */
 public class EsperDomNodeRouteTest extends CamelTestSupport {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(EsperDomNodeRouteTest.class);
