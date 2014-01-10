56df40caff43a3af3437235cdb6b887361a52f8d
cemmersb <cemmersb@gmail.com>
2014-01-10 14:01:10 +0100

    CAMEX-9 - camel-esper endpoint will not load any esper config file even the default one
    FIX: Establish option to differentiate between defaultProvider and defaultConfiguredProvider

--- components/camel-esper/src/main/java/org/apacheextras/camel/component/esper/EsperComponent.java
+++ components/camel-esper/src/main/java/org/apacheextras/camel/component/esper/EsperComponent.java
@@ -1,24 +1,25 @@
-/**************************************************************************************
- http://code.google.com/a/apache-extras.org/p/camel-extra
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- 02110-1301, USA.
-
- http://www.gnu.org/licenses/gpl-2.0-standalone.html
- ***************************************************************************************/
+/**
+ * ************************************************************************************
+ * http://code.google.com/a/apache-extras.org/p/camel-extra
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * http://www.gnu.org/licenses/gpl-2.0-standalone.html
+ * *************************************************************************************
+ */
 package org.apacheextras.camel.component.esper;
 
 import java.util.Map;
@@ -27,6 +28,7 @@ import com.espertech.esper.client.Configuration;
 import com.espertech.esper.client.EPRuntime;
 import com.espertech.esper.client.EPServiceProvider;
 import com.espertech.esper.client.EPServiceProviderManager;
+
 import org.apache.camel.Endpoint;
 import org.apache.camel.impl.DefaultComponent;
 
@@ -36,37 +38,69 @@ import org.apache.camel.impl.DefaultComponent;
  * @version $Revision: 1.1 $
  */
 public class EsperComponent extends DefaultComponent {
-    private EPServiceProvider esperService;
-    private EPRuntime esperRuntime;
 
-    protected Endpoint createEndpoint(String uri, String remaining, Map parameters) throws Exception {
-        return new EsperEndpoint(uri, this, remaining);
+  /**
+   * Event processing service provider.
+   */
+  private EPServiceProvider esperService;
+
+  /**
+   * Esper runtime environment.
+   */
+  private EPRuntime esperRuntime;
+
+  /**
+   * Creates an Esper endpoint. {@inheritDoc}
+   *
+   * @param uri
+   * @param remaining
+   * @param parameters
+   * @return Endpoint
+   * @throws java.lang.Exception
+   */
+  @Override
+  protected Endpoint createEndpoint(String uri, String remaining, Map parameters) throws Exception {
+    return new EsperEndpoint(uri, this, remaining);
+  }
+
+  /**
+   * Returns the event processing service provider. If the provider is null an
+   * instance will be created via the EPServiceProviderManager.
+   *
+   * @return event processing service provider
+   */
+  public EPServiceProvider getEsperService(boolean configured) {
+    if (esperService == null) {
+      if (configured) {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        esperService = EPServiceProviderManager.getProvider("DefaultConfiguredProvider", configuration);
+      } else {
+        esperService = EPServiceProviderManager.getDefaultProvider();
+      }
     }
+    return esperService;
+  }
 
-    public EPServiceProvider getEsperService() {
-        if (esperService == null) {
-            esperService = EPServiceProviderManager.getDefaultProvider();
-        }
-        return esperService;
+  /**
+   * Sets the Esper service provider.
+   *
+   * @param esperService
+   */
+  public void setEsperService(EPServiceProvider esperService) {
+    this.esperService = esperService;
+  }
+
+  /**
+   * Returns the Esper runtime object.
+   *
+   * @return EPRuntime
+   */
+  public EPRuntime getEsperRuntime(boolean configured) {
+    if (esperRuntime == null) {
+      esperRuntime = getEsperService(configured).getEPRuntime();
     }
-
-    public void setEsperService(EPServiceProvider esperService) {
-        this.esperService = esperService;
-    }
-
-    public EPRuntime getEsperRuntime() {
-        if (esperRuntime == null) {
-            esperRuntime = getEsperService().getEPRuntime();
-        }
-        return esperRuntime;
-    }
-
-    @Override
-    protected void doStart() throws Exception {
-        super.doStart();
-
-        // lets force lazy creation
-        getEsperRuntime();
-    }
+    return esperRuntime;
+  }
 
 }
