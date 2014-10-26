fdf544ffe7d4a654bf754a2a27008f4b81b1ad7f
cemmersb <cemmersb@gmail.com>
2014-10-26 12:09:02 -0700

    Add additional test case for Consumer tests

---
+++ components/camel-couchbase/src/test/java/org/apacheextras/camel/component/couchbase/CouchbaseConsumerTest.java
@@ -0,0 +1,53 @@
+/**************************************************************************************
+ http://code.google.com/a/apache-extras.org/p/camel-extra
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ 02110-1301, USA.
+
+ http://www.gnu.org/licenses/gpl-2.0-standalone.html
+ ***************************************************************************************/
+package org.apacheextras.camel.component.couchbase;
+
+import com.couchbase.client.CouchbaseClient;
+import com.couchbase.client.vbucket.ConfigurationException;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.junit.Test;
+
+import java.net.URI;
+import java.util.ArrayList;
+
+
+public class CouchbaseConsumerTest {
+
+  @Test(expected = ConfigurationException.class)
+  public void testNewCouchbaseConsumer() throws Exception{
+    CouchbaseConsumer couchbaseConsumer = new CouchbaseConsumer(
+        new CouchbaseEndpoint(),
+        new CouchbaseClient(
+            new ArrayList<URI>(),
+            "bucketName",
+            "pwd"
+        ),
+        new Processor() {
+          @Override
+          public void process(Exchange exchange) throws Exception {
+            // Nothing to do
+          }
+        }
+    );
+  }
+}
