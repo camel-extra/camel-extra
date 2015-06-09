8c2b324226c3ff9ea8380e52cecf153f2c18d074
cemmersb <cemmersb@gmail.com>
2015-06-09 06:18:24 -0700

    Fix imports and license

--- components/camel-jcifs/src/test/java/org/apacheextras/camel/component/jcifs/FromSmbNotDownloadTest.java
+++ components/camel-jcifs/src/test/java/org/apacheextras/camel/component/jcifs/FromSmbNotDownloadTest.java
@@ -1,46 +1,36 @@
-/**************************************************************************************
+/**
  http://code.google.com/a/apache-extras.org/p/camel-extra
 
  This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public License
- as published by the Free Software Foundation; either version 3
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
  of the License, or (at your option) any later version.
 
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Lesser General Public License for more details.
+ GNU General Public License for more details.
 
 
- You should have received a copy of the GNU Lesser General Public
- License along with this program; if not, write to the Free Software
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  02110-1301, USA.
 
- http://www.gnu.org/licenses/lgpl-3.0-standalone.html
- ***************************************************************************************/
+ http://www.gnu.org/licenses/gpl-2.0-standalone.html
+ */
 package org.apacheextras.camel.component.jcifs;
-
-import java.io.File;
 
 import jcifs.smb.SmbFile;
 import jcifs.smb.SmbFileInputStream;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-
-import org.easymock.EasyMock;
-import org.easymock.IAnswer;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.easymock.EasyMock.anyObject;
-import static org.easymock.EasyMock.createMock;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
-import static org.easymock.EasyMock.verify;
+import static org.easymock.EasyMock.*;
 
 /**
  * Unit test to verify that we can pool an ASCII file from the SMB Server and store it on a local file path
