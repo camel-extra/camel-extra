6aa0d203f10ec3b9bea62997670c22eb0d6f27e6
Christoph Emmersberger <cemmersb@gmail.com>
2015-06-27 14:31:06 -0700

    Add syntax element

--- components/camel-couchbase/src/main/java/org/apacheextras/camel/component/couchbase/CouchbaseEndpoint.java
+++ components/camel-couchbase/src/main/java/org/apacheextras/camel/component/couchbase/CouchbaseEndpoint.java
@@ -41,8 +41,7 @@ import static org.apacheextras.camel.component.couchbase.CouchbaseConstants.*;
 /**
  * Represents a Couchbase endpoint that can query Views with a Poll strategy and/or produce various type of operations.
  */
-
-@UriEndpoint(scheme = "couchbase", consumerClass = CouchbaseConsumer.class)
+@UriEndpoint(scheme = "couchbase", syntax = "couchbase://protocoll/host:port/bucket", consumerClass = CouchbaseConsumer.class)
 public class CouchbaseEndpoint extends ScheduledPollEndpoint {
 
     // URL stuff
